02ae69820d5575f6840d5cb8eb054d1c9519e990
Eddy Lujan <eddylujann@gmail.com>
2023-09-18 00:01:08 +0200

    Practica 5
    Crea un nuevo proyecto y mediante el uso de Stack Views y Constrains, crea una calculadora como la de la imagen.

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UD3_Practica5
+//
+//  Created by Eddy Donald Chinchay Lujan on 8/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
